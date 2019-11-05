c8433384bce430a1c4f79798294ab886234b009d
Dawinder Singh <dawindersingh.orem@gmail.com>
2019-11-05 14:56:58 +0530

    Show toast

--- app/src/main/java/com/myutilslibtest/MainActivity.kt
+++ app/src/main/java/com/myutilslibtest/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.myutilslibtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dawinderutilslib.ToastMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToastMessage.showToast(this, "Hello")
     }
 }
--- dawinderUtilsLib/src/main/java/com/dawinderutilslib/ToastMessage.kt
+++ dawinderUtilsLib/src/main/java/com/dawinderutilslib/ToastMessage.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 
-class ToastMessage {
+object ToastMessage {
 
     fun showToast(c: Context, message: String) {
         Toast.makeText(c, message, Toast.LENGTH_LONG).show()
